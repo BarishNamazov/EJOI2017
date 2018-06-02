6aacaa9fec17cb07e0e395c459e553708a4a2048
BarishNamazov <barish2003@gmail.com>
2018-06-02 16:48:35 +0400

    EJOI2017 Magic solution

--- Magic.cpp
+++ Magic.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int val[128], v[100005], nxt = 0;
 int main() {
     ios_base :: sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0), cout.tie(0);
     int n; string s;
     cin >> n >> s;
     for (int i = 0; i < n; i++) {
